be9f25859257c777f8a122ca4ade2a9597fcb65b
sankaio <kaiofliberal123@gmail.com>
2024-12-10 17:55:57 -0300

    Add files via upload
    parte do Pedro

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Exemplo: Substituir títulos dinamicamente
+document.addEventListener("DOMContentLoaded", () => {
+    const titles = document.querySelectorAll(".news-item h3");
+    titles[0].textContent = "Notícia atualizada sobre o Botafogo!";
+  });
